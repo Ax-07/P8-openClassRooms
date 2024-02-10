97bc5cfe66dcb6188ed8318e54ce1bb09a009f2f
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-02-10 17:20:14 +0100

    ajout lien vers page d'accueil

--- src/pages/Error_404.jsx
+++ src/pages/Error_404.jsx
@@ -1,9 +1,11 @@
+import { NavLink } from "react-router-dom";
+
 export const Error_404 = () => {
     return (
         <div className="error-404">
             <h1 className="error-404__title">404</h1>
             <p className="error-404__text">Oups! La page que vous demandez n'sexiste pas.</p>
-            <p className="error-404__text-sm">Retourner sur la page d’accueil</p>
+            <NavLink to={"/"} className="error-404__link">Retourner sur la page d’accueil</NavLink>
         </div>
     );
 };
